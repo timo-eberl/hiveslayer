7bde5b2912f20867338ef4155d2d71c3ded4c59f
Timo Eberl <timo@timoeberl.de>
2025-05-04 13:27:10 +0200

    fix sound not playing

--- src/bullet.gd
+++ src/bullet.gd
@@ -1,5 +1,9 @@
 extends RigidBody3D
 
 
-func _on_body_entered(_body: Node) -> void:
+func _on_body_entered(body: Node) -> void:
 	queue_free()
+	if body is StaticBody3D:
+		print("ground hit")
+	if body is Enemy:
+		print("enemy hit")
