80b1f11202462c3eee33ac4126ab3f2030b6bdb8
Tripletwinlion <steven.onebamoi@gmail.com>
2025-05-04 13:50:59 +0200

    opening camera animation

---
+++ intro_camera.gd
@@ -0,0 +1,9 @@
+extends Camera3D
+
+func _ready() -> void:
+	$".".position = Vector3(195,2.1,-70)
+
+func _process(delta: float) -> void:
+	if $".".position.z < 0:
+		$".".position.z += 0.25
+		print($".".position.z)
