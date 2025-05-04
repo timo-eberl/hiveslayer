3683d5c707404cbe8a3e2695e93cecb9a9b386fc
Tripletwinlion <steven.onebamoi@gmail.com>
2025-05-04 18:32:47 +0200

    Changed fog + timer

--- control.gd
+++ control.gd
@@ -2,9 +2,13 @@ extends Control
 
 @onready var player = %Player
 @onready var playerCamera = %Camera
+@onready var timer = %Timer
+@onready var time_Label = %TimeSurvived
+@onready var visible_timer = %CanvasTime
 
 #Checks if the game has started
 var started := false
+var total_time_in_seconds = 0
 
 #Checks if the player is allowed to rotate
 #Should probably rename it to pause.
@@ -19,6 +23,9 @@ func _on_button_pressed():
 	pause = true
 	$".".visible = false
 	playerCamera.make_current()
+	timer.start()
+	visible_timer.visible = true
+	
 
 func pauze_game():
 	player.enabled = false
@@ -26,3 +33,12 @@ func pauze_game():
 	
 
 	
+
+
+func _on_timer_timeout() -> void:
+	total_time_in_seconds += 1
+	var m = int(total_time_in_seconds/60.0)
+	var s = total_time_in_seconds - m * 60
+	time_Label.text = 'Seconds survived: %02d:%02d' % [m, s]
+	
+	pass # Replace with function body.
